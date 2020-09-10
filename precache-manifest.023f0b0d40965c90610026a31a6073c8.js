d7dcf58e636f413548da567db6ac01db332d9bde
AhKhai <seanchow1999@gmail.com>
2020-09-10 16:10:48 +0800

    Updates

---
+++ precache-manifest.023f0b0d40965c90610026a31a6073c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893da76d2ed3984f306bc846421a8e19",
+    "url": "/ReactToDoList/index.html"
+  },
+  {
+    "revision": "f3f670f936fb50bef09e",
+    "url": "/ReactToDoList/static/css/main.7324e5cf.chunk.css"
+  },
+  {
+    "revision": "267b3a5a321546bce500",
+    "url": "/ReactToDoList/static/js/2.18a333c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactToDoList/static/js/2.18a333c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f670f936fb50bef09e",
+    "url": "/ReactToDoList/static/js/main.0233b9cc.chunk.js"
+  },
+  {
+    "revision": "824f21ac4a1cbb0ae4ca",
+    "url": "/ReactToDoList/static/js/runtime-main.5647f28f.js"
+  }
+]);
